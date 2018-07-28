e45dd0a78f01393257c713e635af0badb43d709d
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-28 13:23:18 +0530

    Added Routes Folder for Dedicated Routes, Created GET Route, POST route and Delete Route.

---
+++ routes/api/items.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router()
+const Item = require('../../models/Item')
+
+
+// @route  GET api/items
+// @desc   Get all Items from Database
+// @access Public
+
+router.get('/' , (req,res) => {
+
+Item
+.find({})
+.sort({date : -1}) // descending sort
+.then(items => { res.json (items)})
+
+
+})
+
+
+
+// @route  POST api/items
+// @desc   Create an Item / Post to Database
+// @access Public
+
+router.post('/' , (req,res) => {
+
+const newItem = new Item({
+  name: req.body.name,
+})
+
+newItem.save().then(item => res.json(item))
+
+})
+
+
+// @route  Delete api/items/:id
+// @desc   Delete An Item from Database
+// @access Public
+
+router.delete('/:id' , (req,res) => {
+
+Item
+.findById(req.params.id)
+.then(item => item.remove().then( () => res.json({success : true} )))
+.catch( (err) => { res.status(404).json({success: false})} )
+
+})
+
+
+
+module.exports = router;
