256f6f2edaa84a82d11b10fe8960cda0c4525407
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-10-08 17:30:03 +0530

    Digital Cloud Deployment

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -7,7 +7,7 @@ export const getItems = () => dispatch => {
   dispatch(setItemsLoading());
   // could use fetch, instead, to make the http request ...
 axios
-  .get('http://159.65.155.169:5000/api/items')
+  .get('/api/items')
   .then(res => dispatch({
     type: GET_ITEMS,
     payload: res.data
@@ -18,7 +18,7 @@ axios
 // Sending / Dispatching Payload to Reducers
 
 export const deleteItem = (id) => dispatch => {
-axios.delete(`http://159.65.155.169:5000/api/items/${id}`)
+axios.delete(`api/items/${id}`)
 .then(res =>  dispatch({
   type: DELETE_ITEM,
   payload: id
@@ -26,7 +26,7 @@ axios.delete(`http://159.65.155.169:5000/api/items/${id}`)
 }
 
 export const addItem = (item) => dispatch => {
-axios.post('http://159.65.155.169:5000/api/items' , item)
+axios.post('/api/items' , item)
 .then(res => dispatch({
     type: ADD_ITEM,
     payload: res.data
