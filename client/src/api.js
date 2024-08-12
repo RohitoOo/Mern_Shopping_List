0cbac4259fc816f276d8fe227b67b2e5c3d03509
RohitoOo <36753710+RohitoOo@users.noreply.github.com>
2024-08-11 23:53:14 -0400

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_ROOT = "https://fullstack-mern.herokuapp.com"
+const API_ROOT = "https://git.heroku.com/mern-shoppinglist.git"
 axios.defaults.baseURL = API_ROOT
 
 export { API_ROOT }
